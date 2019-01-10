6bea4dca61a80115a5d877871cf92c2ce337d16d
Rohit Agarwal <programmingwithrohit@gmail.com>
2019-01-10 23:28:46 +0530

    Project structure

---
+++ src/main/java/com/javamultiplex/App.java
@@ -0,0 +1,13 @@
+package com.javamultiplex;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
