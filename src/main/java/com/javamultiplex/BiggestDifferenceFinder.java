31eee6385e805fb3ea3381a40d80a3c24d55f222
Rohit Agarwal <programmingwithrohit@gmail.com>
2019-01-11 00:37:27 +0530

    Improved code to get biggest diff between two array elements

--- src/main/java/com/javamultiplex/BiggestDifferenceFinder.java
+++ src/main/java/com/javamultiplex/BiggestDifferenceFinder.java
@@ -1,28 +1,22 @@
 package com.javamultiplex;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
+import java.util.function.Supplier;
+import java.util.stream.IntStream;
 
-
+/**
+ * @author Rohit Agarwal
+ */
 public class BiggestDifferenceFinder {
 
     /**
-     *
      * @param arr
      * @return
      */
     public int getBiggestDiffBetweenTwoArrayElements(int[] arr) {
-        int length = arr.length;
-        int diff = 0;
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < length; i++) {
-            for (int j = i + 1; j < length; j++) {
-                diff = Math.abs(arr[i] - arr[j]);
-                list.add(diff);
-            }
-        }
-        Collections.sort(list);
-        return list.get(list.size() - 1);
+        Supplier<IntStream> streamSupplier = () -> Arrays.stream(arr);
+        int max = streamSupplier.get().max().getAsInt();
+        int min = streamSupplier.get().min().getAsInt();
+        return max - min;
     }
 }
